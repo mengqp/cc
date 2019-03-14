04867eb342e0c1b9c5fa91458aa601f09ca28721
mengqp <meng_qingpu@126.com>
2019-03-14 10:15:19 +0800

    修改名字

--- sine/sine.c
+++
@@ -1,8 +0,0 @@
-/**
- *   \file sine.c
- *   \brief 实现sine 产生波形
- *
- *  Detailed description
- *
- */
-
---
+++ sinewave/sinewave.c
@@ -0,0 +1,14 @@
+/**
+ *   \file sinewave.c
+ *   \brief 实现sine 产生波形
+ *
+ *  Detailed description
+ *
+ */
+
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define  PI 3.14159265259
